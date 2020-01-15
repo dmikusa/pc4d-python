f5e97b2247744642247364c2ac85dc4df98cffa8
Daniel Mikusa <dan@mikusa.com>
2020-01-15 17:00:28 -0500

    Switch to using Python module libnacl

--- src/encrypt.py
+++ src/encrypt.py
@@ -3,8 +3,7 @@
 # Use symetric encryption to encrypt a value
 #
 import sys
-import nacl.secret
-import nacl.utils
+import libnacl.secret
 from base64 import b64encode
 
 
@@ -14,21 +13,22 @@ if __name__ == '__main__':
               .format(sys.argv[0]))
         sys.exit(1)
 
-    # generate a key to use
-    key = nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
-
     # message must be of type bytes, not str, so we convert
     message = sys.argv[1].encode()
 
-    # create a box from our key & encrypt the message
-    #  a random nonce is automatically generated
-    box = nacl.secret.SecretBox(key)
-    encrypted = box.encrypt(message)
+    # create a secret box, which automatically creates a nonce & key
+    box = libnacl.secret.SecretBox()
+    enc_bytes = box.encrypt(message)
 
-    print("Nonce: {}".format(encrypted.nonce.hex()))
-    print("Key: {}".format(key.hex()))
-    print("Box: {}".format(b64encode(encrypted).decode("utf-8")))
+    # nonce is the first part of the encrypted data, so we strip it out
+    #  this is purely just to print it below
+    nonce = enc_bytes[0:libnacl.crypto_secretbox_NONCEBYTES]
+
+    print("Nonce: {}".format(nonce.hex()))
+    print("Key: {}".format(box.hex_sk().decode('utf-8')))
+    print("Box: {}".format(b64encode(enc_bytes).decode('utf-8')))
     print()
-    print("Decrypt with: python src/decrypt.py {} {}".format(key.hex(),
-          b64encode(encrypted).decode("utf-8")))
+    print("Decrypt with: python src/decrypt.py {} {}".format(
+          box.hex_sk().decode('utf-8'),
+          b64encode(enc_bytes).decode("utf-8")))
     print()
